bee0c84d06a4b9377ea6c1764f5937b96954fdaa
william duan <38791932+duanwilliam@users.noreply.github.com>
2021-06-20 23:01:23 -0700

    fix: fix href for Similar mdx component on build

--- src/mdx/Similar/index.jsx
+++ src/mdx/Similar/index.jsx
@@ -5,7 +5,7 @@ import { Easy, Medium, Hard } from '..';
 const Similar = (props) => {
   const { title, e, m, h } = props;
   const Tag = e ? Easy : m ? Medium : h ? Hard : 'div';
-  const href = `./${title.toLowerCase().replaceAll(/\s+/g, '-')}`;
+  const href = `../${title.toLowerCase().replace(/\s+/g, '-')}`;
   return (
     <Tag>
       <p>
